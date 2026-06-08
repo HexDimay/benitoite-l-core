d3f0a5fec0f7ae27194806355c75787e0c5cd552
0xDimay <HexDimay@yandex.ru>
2026-06-08 19:46:40 +0300

    .

--- src/sql/mod.rs
+++ src/sql/mod.rs
@@ -8,6 +8,8 @@ pub struct SqlState {
 }
 
 impl SqlState {
+    /// Осуществляет слединение с базой данных.
+    /// Если SQL уже подключён, он отключается от базы данных и подключается к указанной базе данных.
     pub async fn connect(&mut self, select_db: MetadataDataBase) -> anyhow::Result<()> {
         if let Some(_) = &self.sqlite_connection {
             self.clouse().await?;
@@ -15,14 +17,16 @@ impl SqlState {
 
         let connection =
             sqlx::SqliteConnection::connect(&select_db.path().to_string_lossy()).await?;
+        log::info!("Connected database: {}", select_db.name());
         self.sqlite_connection = Some(connection);
         Ok(())
     }
 
+    /// Осуществляет выход и закрытие базы данных.
     pub async fn clouse(&mut self) -> anyhow::Result<()> {
         if let Some(c) = std::mem::take(&mut self.sqlite_connection) {
             c.close().await?;
-            log::info!("");
+            log::info!("Closing the database connection.");
         }
 
         Ok(())
